5af09d41a97b677e528e3bc518a87b69431dfe4d
Swapnil Sharma <swapnil@hippocraticai.com>
2025-01-30 13:42:19 -0800

    Added home page

--- server.py
+++ server.py
@@ -12,5 +12,29 @@ def handle_xml_request():
     # Return XML response with proper content type
     return Response(xml_string, mimetype='application/xml')
 
+@app.route('/', methods=['GET'])
+def home():
+    return '''
+    <html>
+        <head>
+            <title>XML Server</title>
+        </head>
+        <body>
+            <h1>Welcome to the XML Server</h1>
+            <p>This server provides XML responses via POST requests to the /xml endpoint.</p>
+            <h2>Usage:</h2>
+            <pre>
+POST /xml
+Content-Type: application/json
+
+{
+    "name": "John",
+    "age": "30"
+}
+            </pre>
+        </body>
+    </html>
+    '''
+
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
